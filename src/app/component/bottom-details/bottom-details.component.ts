731c2a32457b91eedbe1ef886bf39f5ba22b1ccc
OmriKellnerMoveo <omrik@moveo.co.il>
2022-04-24 14:47:37 +0300

    delete comments. create interface & service directory optimization code removed duplicate-code using map_service rename files change css ->scss remove un-use files

--- src/app/component/bottom-details/bottom-details.component.ts
+++ src/app/component/bottom-details/bottom-details.component.ts
@@ -1,16 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-bottom-details',
   templateUrl: './bottom-details.component.html',
   styleUrls: ['./bottom-details.component.css']
 })
-export class BottomDetailsComponent implements OnInit {
+export class BottomDetailsComponent {
   @Input() element: any
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
